43431ba99752b46a3ccdb968db764ac02c29ce99
timjj92 <timjj92@gmail.com>
2019-11-12 17:20:58 -0800

    added search bar

--- client/containers/MainContainer.jsx
+++ client/containers/MainContainer.jsx
@@ -24,7 +24,8 @@ import {
 const MainContainer = () => {
   //instantiating hook in this component so that the fetch occurs earlier
   const [news, newsUpdate] = useState([[{title:'Loading...', link:'#'}],[{title:'Loading...', link:'#'}],[{title:'Loading...', link:'#'}]]);
-  
+  const [location, setLoc] = useState("");
+
   // upon rendering, the fetch will occur and the hook 'newsUpdate' should update the state
   useEffect(() => {
     fetch('/news')
@@ -37,58 +38,63 @@ const MainContainer = () => {
         console.log(err)
     })
 },[])
-    //easter egg for sandstorm- just need to click the fire icon
-    const Easteregg = () => {
-      const audio = new Audio(
-        'https://iringtone.net/rington/file?id=8454&type=sound&name=mp3'
-      );
-      audio.play();
-      const app = document.getElementById('root');
-      app.classList.add('easter-egg');
-      const colorGen = () => {
-        const r = Math.floor(Math.random() * 256);
-        const g = Math.floor(Math.random() * 256);
-        const b = Math.floor(Math.random() * 256);
-        return 'rgb(' + r + ',' + g + ',' + b + ')';
-      };
-      setInterval(() => {
-        const elements = document.querySelectorAll('*');
-        for (let i = 0; i < elements.length; i += 1) {
-          elements[i].style.backgroundColor = `${colorGen()}`;
-          elements[i].style.color = `${colorGen()}`;
-          elements[i].style.fill = `${colorGen()}`;
-        }
-      }, 100);
-    };
 
-    return ( 
-        <article id ="mainContainer">
-            <Router>
-             <Navbar bg="dark" variant="dark">
-                <Navbar.Brand>
-                <img
-                    alt=""
-                    src="http://www.clker.com/cliparts/P/r/s/n/g/W/maron-flame-logo-4.svg"
-                    width="30"
-                    height="30"
-                    className="d-inline-block align-top"
-                    onClick={()=> Easteregg()}
-                />
-                <Link className="navLinks" to="/">Verifire</Link>
-                </Navbar.Brand>
-                <NavLink className="navLinks" to="/main">Content</NavLink>
-            </Navbar>
-            <Switch>
-          <Route path="/main">
-            <ContentContainer news={news}/>
-          </Route>
-          <Route path="/">
-            <LandingContainer/>
-          </Route>
-          </Switch>
-        </Router>
-        </article>
-     );
+  const updateLocation = (newLoc) =>{
+    console.log(newLoc);
+    setLoc(newLoc);
+  };
+    //easter egg for sandstorm- just need to click the fire icon
+  const Easteregg = () => {
+    const audio = new Audio(
+      'https://iringtone.net/rington/file?id=8454&type=sound&name=mp3'
+    );
+    audio.play();
+    const app = document.getElementById('root');
+    app.classList.add('easter-egg');
+    const colorGen = () => {
+      const r = Math.floor(Math.random() * 256);
+      const g = Math.floor(Math.random() * 256);
+      const b = Math.floor(Math.random() * 256);
+      return 'rgb(' + r + ',' + g + ',' + b + ')';
+    };
+    setInterval(() => {
+      const elements = document.querySelectorAll('*');
+      for (let i = 0; i < elements.length; i += 1) {
+        elements[i].style.backgroundColor = `${colorGen()}`;
+        elements[i].style.color = `${colorGen()}`;
+        elements[i].style.fill = `${colorGen()}`;
+      }
+    }, 100);
+  };
+
+  return ( 
+    <article id ="mainContainer">
+        <Router>
+          <Navbar bg="dark" variant="dark">
+            <Navbar.Brand>
+            <img
+                alt=""
+                src="http://www.clker.com/cliparts/P/r/s/n/g/W/maron-flame-logo-4.svg"
+                width="30"
+                height="30"
+                className="d-inline-block align-top"
+                onClick={()=> Easteregg()}
+            />
+            <Link className="navLinks" to="/">Verifire</Link>
+            </Navbar.Brand>
+            <NavLink className="navLinks" to="/main">Content</NavLink>
+          </Navbar>
+          <Switch>
+        <Route path="/main">
+          <ContentContainer news={news}/>
+        </Route>
+        <Route path="/">
+          <LandingContainer updateLocation={updateLocation}/>
+        </Route>
+        </Switch>
+      </Router>
+    </article>
+  );
 }
 
 //
