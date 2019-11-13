352d2b24ecc8a3b115b30391af1c3aca043a461c
timjj92 <timjj92@gmail.com>
2019-11-12 19:07:59 -0800

    changed org name to teamd-umbledore

--- client/containers/LandingContainer.jsx
+++ client/containers/LandingContainer.jsx
@@ -8,100 +8,100 @@ import Particles from 'react-particles-js'
 //the particles in this container are for the polygon mask before entering the page
 
 const LandingContainer = (props) => {
-    const [location, setTempLoc] = useState("");
-    
-    const tempLoc = (e) =>{
-        console.log(e.target.value);
-        setTempLoc(e.target.value);
-    }
+  const [location, setTempLoc] = useState("");
+  
+  const tempLoc = (e) =>{
+    console.log(e.target.value);
+    setTempLoc(e.target.value);
+  }
 
-    return ( 
-        <div id="landing">
-            <div id="landingInfo">
-                <h2>DisasterDash:</h2>
-                <span>Keep Up With The Disaster</span>
-                <p>Stay updated, know your coverage, and keep connected</p>
-                <input onChange= {tempLoc} type="text"></input><button onClick={() => props.updateLocation(location)}>Search</button>
-            </div>
-            <div id="particles">
-            <Particles
-                params={{
-                    // Maximum Frames Per Second
-                    "fps_limit": 60,
-                    "particles": {
-                        "number": {
-                            // Number of Particles
-                            "value": 600,
-                            "density": {
-                                "enable": false
-                            }
-                        },
-                        // Lines between Particles
-                        "line_linked": {
-                            "enable": true,
-                            // Maximum Distance that Particles will attach to each other
-                            "distance": 20,
-                            "opacity": 0.4
-                        },
-                        // Movement Speed of the Particles
-                        "move": {
-                            "speed": 10
-                        },
-                        "opacity": {
-                            "anim": {
-                                "enable": true,
-                                "opacity_min": 0.05,
-                                "speed": 2,
-                                "sync": false
-                            },
-                            "value": 0.4
-                        }
-                    },
-                    "polygon": {
-                        "enable": true,
-                        // Size of your mask
-                        "scale": 0.55,
-                        // Type can be:
-                        // inline(particles on the lines of your mask),
-                        // inside(particles inside the lines of your mask[ Will only work on non-curved lines ]),
-                        // outside(particles outside the lines of your mask[ Will only work on non-curved lines ])
-                        "type": "inline",
-                        "move": {
-                            // How far away from your lines the particles are allowed to venture out
-                            "radius": 5
-                        },
-                        // Url for mask, here we are routing to the backend
-                        "url":"/flare",
-                        "inline": {
-                            "arrangement": "equidistant"
-                        },
-                        "draw": {
-                            "enable": false,
-                            "stroke": {
-                                "color": "rgba(0, 0, 0, .2)"
-                            }
-                        }
-                    },
-                    "retina_detect": false,
-                    "interactivity": {
-                        "events": {
-                            "onhover": {
-                                "enable": true,
-                                "mode": "bubble"
-                            }
-                        },
-                        "modes": {
-                            "bubble": {
-                                "size": 6,
-                                "distance": 40
-                            }
-                        }
-                    }
-                }} />
-                </div>
-                <p id="company">by Magitech</p>
-        </div>
-     );
+  return ( 
+    <div id="landing">
+      <div id="landingInfo">
+        <h2>DisasterDash:</h2>
+        <span>Keep Up With The Disaster</span>
+        <p>Stay updated, know your coverage, and keep connected</p>
+        <input onChange= {tempLoc} type="text"></input><button onClick={() => props.updateLocation(location)}>Search</button>
+      </div>
+      <div id="particles">
+      <Particles
+          params={{
+            // Maximum Frames Per Second
+            "fps_limit": 60,
+            "particles": {
+              "number": {
+                // Number of Particles
+                "value": 600,
+                "density": {
+                  "enable": false
+                }
+              },
+              // Lines between Particles
+              "line_linked": {
+                "enable": true,
+                // Maximum Distance that Particles will attach to each other
+                "distance": 20,
+                "opacity": 0.4
+              },
+              // Movement Speed of the Particles
+              "move": {
+                "speed": 10
+              },
+              "opacity": {
+                "anim": {
+                  "enable": true,
+                  "opacity_min": 0.05,
+                  "speed": 2,
+                  "sync": false
+                },
+                "value": 0.4
+              }
+            },
+            "polygon": {
+              "enable": true,
+              // Size of your mask
+              "scale": 0.55,
+              // Type can be:
+              // inline(particles on the lines of your mask),
+              // inside(particles inside the lines of your mask[ Will only work on non-curved lines ]),
+              // outside(particles outside the lines of your mask[ Will only work on non-curved lines ])
+              "type": "inline",
+              "move": {
+                // How far away from your lines the particles are allowed to venture out
+                "radius": 5
+              },
+              // Url for mask, here we are routing to the backend
+              "url":"/flare",
+              "inline": {
+                "arrangement": "equidistant"
+              },
+              "draw": {
+                "enable": false,
+                "stroke": {
+                  "color": "rgba(0, 0, 0, .2)"
+                }
+              }
+            },
+            "retina_detect": false,
+            "interactivity": {
+              "events": {
+                "onhover": {
+                  "enable": true,
+                  "mode": "bubble"
+                }
+              },
+              "modes": {
+                "bubble": {
+                  "size": 6,
+                  "distance": 40
+                }
+              }
+            }
+        }} />
+      </div>
+      <p id="company">by teamd-Umbledore</p>
+    </div>
+  );
 }
  
 export default LandingContainer;
