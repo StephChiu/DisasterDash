9ede998a773317786836d0811d7dee63b65f099a
timjj92 <timjj92@gmail.com>
2019-11-13 20:10:37 -0800

    removed alert fetch

--- client/components/Maps.jsx
+++ client/components/Maps.jsx
@@ -1,34 +1,36 @@
-import React from 'react';
+import React, {useEffect}from 'react';
 
 
 //This componenet was build for the purpose of displaying a map that displays local disasters
 //We started with displaying fires
 
-const Maps = (props, location) => {
-    console.log(location)
+const Maps = (props) => {
     let latitude
     let longitude
-    fetch('/chooseLoc/:name')
-    .then(data => data.json())
-    .then(data => {
-        console.log(data)
-        latitude = data.lat;
-        longitude = data.lng;
-    })
-    .then(() => {
-        console.log(latitude);
-        const aeris = new AerisWeather('BX5DJsHFWzOBmPsfAVf15', 'p7wb1Z4XFlC5g7NrLsQOmqNIBlr7JDUUypuW3Opb');
-        aeris.views().then(views => {
-            const map = new views.InteractiveMap(document.getElementById('map'), {
-                center: {
-                    lat: latitude,
-                    lon: longitude
-                },
-                zoom: 11,
-                layers: 'fires-outlook,fires-dryltg-outlook,fires-obs-icons,lightning-strike-density,fires-obs-points,alerts,stormcells,tropical-cyclones'
+    useEffect(()=>{
+        console.log(props);
+        fetch(`/chooseLoc/${props.location}`)
+        .then(data => data.json())
+        .then(data => {
+            console.log(data)
+            latitude = data.lat;
+            longitude = data.lng;
+        })
+        .then(() => {
+            console.log(latitude);
+            const aeris = new AerisWeather('BX5DJsHFWzOBmPsfAVf15', 'p7wb1Z4XFlC5g7NrLsQOmqNIBlr7JDUUypuW3Opb');
+            aeris.views().then(views => {
+                const map = new views.InteractiveMap(document.getElementById('map'), {
+                    center: {
+                        lat: latitude,
+                        lon: longitude
+                    },
+                    zoom: 11,
+                    layers: 'fires-outlook,fires-dryltg-outlook,fires-obs-icons,lightning-strike-density,fires-obs-points,alerts,stormcells,tropical-cyclones'
+                });
             });
-        });
-    })
+        })
+    },[props.location]);
     
 
     return ( 
