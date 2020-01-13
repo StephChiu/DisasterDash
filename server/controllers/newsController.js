647bdc14b3d523a352c571b49dea1f9c41771c50
Stephanie Chiu <stephaniekchiu@gmail.com>
2020-01-12 19:48:37 -0800

    dotenv api key

--- server/controllers/newsController.js
+++ server/controllers/newsController.js
@@ -1,14 +1,16 @@
 const fetch = require('node-fetch');
 const axios = require('axios')
 const newsController = {};
+require('dotenv').config();
 
 //getNews middleware scrapes titles and links from source sites, as specified in server.js
 newsController.getNews = (req, res, next) => {
+  let newsKey1 = process.env.NEWS_APIKEY1
   let location = req.query.loc;
   location = location.replace(" ", "+");
   let disaster = req.query.dis;
   disaster = disaster.replace(" ", "+");
-  const fetchAdd = `https://newsapi.org/v2/everything?qInTitle=${location}+${disaster}&sortBy=publishedAt&apiKey=27c7768c7a1044f9aeee364a9a5f6dfd`;
+  const fetchAdd = `https://newsapi.org/v2/everything?qInTitle=${location}+${disaster}&sortBy=publishedAt&apiKey=${newsKey1}`;
   fetch(fetchAdd)
   .then(body => body.json())
   .then(body =>{
@@ -20,9 +22,10 @@ newsController.getNews = (req, res, next) => {
 //getAlerts middleware scrapes top alerts from LAFD
   //this returns a single array of objects; not nested as returned by .getNews, as it's only scraping from one source
 newsController.getAlerts = (req, res, next) => {
+  let newsKey2 = process.env.NEWS_APIKEY2
   const inputLocation = req.query.location || 'LosAngeles';
   const disaster = req.query.disaster;
-  const URL = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${disaster}%7C|${inputLocation}&type=video&key=AIzaSyB6prpiKLN2RWqJEwBQoOdYKZs-KmO66O4`
+  const URL = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${disaster}%7C|${inputLocation}&type=video&key=${newsKey2}`
   axios.get(URL)
     .then(response => {
       console.log('GET ALERTS CONTROLLER -> ', response.data.items);
